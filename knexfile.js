82d8accab9bfa1494f5cb29ec9cacdeb8ce98495
Tom Conchie <tconchie@gmail.com>
2017-05-17 10:36:54 -0600

    Finishes part 1

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,21 @@
 
 module.exports = {
 
-  development: { },
-  test: { },
-  production: { },
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_URL || 'classified_dev',
+    }
+  },
+  test: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_URL || 'classified_test',
+    }
+  },
+  "production": {
+    "client": "pg",
+    "connection": process.env.DATABASE_URL
+  }
 
 };
